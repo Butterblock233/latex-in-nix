c6012b4db81b4d57abd301fcd82a8af6408974e1
Butterblock233 <Voltage15312@outlook.com>
2025-12-13 18:33:08 +0000

    init
    tmp: main tex

---
+++ default.nix
@@ -0,0 +1,38 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+
+pkgs.stdenv.mkDerivation {
+  name = "pdf";
+  src = ./.;
+  buildInputs = with pkgs; [
+    (texlive.combine {
+      inherit (texlive)
+        latexmk
+        scheme-basic
+        biblatex
+        xetex
+        biber
+        fontspec
+        ctex
+        enumitem
+        amsmath
+        geometry
+        xcolor
+        listings
+        ;
+    })
+    source-han-serif
+    source-han-sans
+
+  ];
+  buildPhase = ''
+    mkdir -p .cache/latex
+    latexmk -interaction=nonstopmode -auxdir=.cache/latex -xelatex main.tex
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp main.pdf $out
+  '';
+}
