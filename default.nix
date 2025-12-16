dd2951ae925009f22c24f222bcf2aa5d5fc79e95
Butterblock233 <Voltage15312@outlook.com>
2025-12-16 16:23:30 +0000

    fix: update comments and docs for nix file

--- default.nix
+++ default.nix
@@ -10,33 +10,26 @@ pkgs.stdenv.mkDerivation {
   src = ./.;
   buildInputs = pkgsConfig.buildInputs;
 
-  # 确保 texlive 能找到字体
   FONTCONFIG_FILE = pkgsConfig.fontConfig;
 
-  # 设置可写的缓存目录
   preBuild = ''
-    # 在沙盒环境中设置可写目录
     export HOME=$TMPDIR/home
     mkdir -p $HOME
 
-    # 设置字体缓存目录
     export XDG_CACHE_HOME=$TMPDIR/cache
     mkdir -p $XDG_CACHE_HOME/fontconfig
 
-    # 设置 LaTeX 搜索路径，包含当前目录
     export TEXINPUTS=".:$TEXINPUTS"
 
 
   '';
 
   buildPhase = ''
-    # 创建缓存目录
     mkdir -p .cache/latex
 
-    # 使用 xelatex 构建
+    # build LateX document
     latexmk -interaction=nonstopmode -auxdir=.cache/latex -xelatex main.tex
 
-    # 如果需要 biber 处理参考文献，取消下面两行的注释：
     # biber main
     # xelatex -interaction=nonstopmode main.tex
   '';
--- pkgs.nix
+++ pkgs.nix
@@ -3,13 +3,15 @@
 
 let
   fonts = with pkgs; [
+    # add your extra fonts below
     corefonts
     noto-fonts-cjk-serif-static
     source-han-serif
     source-han-sans
     jetbrains-mono
   ];
-
+  # TexLive package is huge, so we combined TexLive basic packages with extra packages we need.
+  # If you hate add package each time you need it, you can just edit sheme package to `scheme-full`. available schemes: scheme-mininal, scheme-basic, scheme-small, scheme-medium, scheme-full, etc.
   texlivePkgs = pkgs.texlive.combine {
     inherit (pkgs.texlive)
       scheme-full
@@ -29,14 +31,16 @@ let
       pdfcol
       fandol
       minted
+      # add your extra texlive packages before `;`
       ;
   };
 
   buildInputs = with pkgs; [
-    texlivePkgs
+    texlivePkgs # combined TexLive packages
     python313Packages.pygments
     fontconfig
     fonts
+    # add your extra packages below
   ];
 
   fontConfig = pkgs.makeFontsConf { fontDirectories = fonts; };
