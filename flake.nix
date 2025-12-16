edb0acf330989479f2a07056c2277093a0d9b46f
Butterblock233 <Voltage15312@outlook.com>
2025-12-16 05:18:38 +0000

    feat: add basic and full template

--- flake.nix
+++ flake.nix
@@ -1,16 +1,44 @@
 {
   description = "A simple LaTeX template for writing documents with latexmk";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         packages.default = pkgs.callPackage ./default.nix { };
-      });
+      }
+    ))
+    // {
+      templates = {
+        full = {
+          path = ./.;
+          description = "The default LaTeX template";
+          welcomeText = ''
+            # LaTeX Template initialized!
+            Run `nix build` to compile the document.
+          '';
+        };
+        basic = {
+          path = ./templates/basic;
+          description = "The default LaTeX template";
+          welcomeText = ''
+            # LaTeX Template initialized!
+            Run `nix build` to compile the document.
+          '';
+        };
+      };
+    };
 }
---
+++ templates/basic/default.nix
@@ -0,0 +1,45 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+
+let
+  fonts = with pkgs; [
+	# add extra fonts here
+  ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "pdf";
+  src = ./.;
+  buildInputs = with pkgs; [
+    # 为了节省磁盘空间使用了基础texlive加上自定义包
+    # 在最底部添加新包重新nix build即可
+    # 如果觉得过于繁琐可以把整个(texlive.combine .....)换成texliveFull
+    (texlive.combine {
+      inherit (texlive)
+        latexmk
+        scheme-basic
+        biblatex
+        xetex
+        biber
+        fontspec
+        ctex
+        # add your custom package here
+        ;
+    })
+    python313Packages.pygments
+    fontconfig
+    fonts
+  ];
+  # ensure texlive can find fonts
+  FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = fonts; };
+
+  buildPhase = ''
+    mkdir -p .cache/latex
+    latexmk -interaction=nonstopmode -auxdir=.cache/latex -xelatex main.tex
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp main.pdf $out
+  '';
+}
