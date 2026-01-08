4af95dd47029b23fc12ee04ae38ac4a37aca2a62
Butterblock233 <Voltage15312@outlook.com>
2026-01-08 03:48:26 +0000

    feat: add some package collections

--- pkgs.nix
+++ pkgs.nix
@@ -5,33 +5,28 @@ let
   fonts = with pkgs; [
     # add your extra fonts below
     corefonts
-    noto-fonts-cjk-serif-static
     source-han-serif
     source-han-sans
-    jetbrains-mono
   ];
   # TexLive package is huge, so we combined TexLive basic packages with extra packages we need.
   # If you hate add package each time you need it, you can just edit sheme package to `scheme-full`. available schemes: scheme-mininal, scheme-basic, scheme-small, scheme-medium, scheme-full, etc.
   texlivePkgs = pkgs.texlive.combine {
     inherit (pkgs.texlive)
-      scheme-full
+      scheme-medium
+      collection-latex
+      collection-latexrecommended
+      collection-latexextra
+      collection-fontsrecommended
+      collection-fontsextra
+      collection-langenglish
+      collection-langeuropean
+      collection-langchinese
+      collection-langjapanese
+
       latexmk
-      biblatex
-      xetex
       biber
-      fontspec
-      ctex
-      everypage
-      titlesec
-      tcolorbox
-      eso-pic
-      fancyhdr
-      mdframed
-      tikzfill
-      pdfcol
-      fandol
-      minted
       # add your extra texlive packages before `;`
+
       ;
   };
 
